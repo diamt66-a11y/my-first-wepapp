88af36b27c772964681ae22766a840d099c10e72
diamt66-a11y <diamt66@gmail.com>
2026-05-25 14:16:12 +0900

    서버 오류 수정 완료

--- api/chat.js
+++ api/chat.js
@@ -1,6 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
   }
