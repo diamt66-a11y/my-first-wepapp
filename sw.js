980df3b98e031aa1647454b9ba4ed3e3fbff6c3e
diamt66-a11y <diamt66@gmail.com>
2026-05-25 12:23:30 +0900

    진짜 핸드폰 앱(PWA) 기능 추가!

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'vibe-coding-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icon.png',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
